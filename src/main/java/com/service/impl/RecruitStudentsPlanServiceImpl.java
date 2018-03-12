2e1931fa24998182d0da1a63bb702816368924b4
84548 <845486067@>
2018-03-12 20:40:50 +0800

    新增数据上传实现方法 uploadData

---
+++ src/main/java/com/service/impl/RecruitStudentsPlanServiceImpl.java
@@ -0,0 +1,171 @@
+package com.service.impl;
+
+import com.common.Constants;
+import com.common.result.CodeMsg;
+import com.dao.RecruitStudentsPlanMapper;
+import com.entity.RecruitStudentsPlan;
+import com.service.RecruitStudentsPlanService;
+import com.util.excel.AbstractExcelUtil;
+import com.util.exception.CustomException;
+import com.util.normal.CommonUtils;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author 潘根山
+ * @create 2018-03-12 19:45
+ * @since 1.0.0
+ */
+@Service
+public class RecruitStudentsPlanServiceImpl implements RecruitStudentsPlanService {
+    @Autowired
+    private RecruitStudentsPlanMapper recruitStudentsPlanMapper;
+
+    @Override
+    public void uploadData(MultipartFile file) throws IOException, CustomException {
+        InputStream inp = file.getInputStream();
+        String fileName = file.getOriginalFilename();
+        Workbook workbook = AbstractExcelUtil.getExcel(inp, fileName);
+        if (workbook == null) {
+            throw new CustomException(CodeMsg.FILE_DATA_EMPTY.getMsg());
+        }
+        Sheet sheet = workbook.getSheetAt(0);
+        Row row;
+        List<RecruitStudentsPlan> recruitStudentsPlans = new ArrayList<>();
+        RecruitStudentsPlan recruitStudentsPlan;
+        for (int i = 1, len = sheet.getLastRowNum(); i <= len; i++) {
+            row = sheet.getRow(i);
+            recruitStudentsPlan = new RecruitStudentsPlan();
+            for (int j = 0; j < 31; j++) {
+                String v = AbstractExcelUtil.getCellByType(row.getCell(j));
+                switch (j) {
+                    case 0:
+                        recruitStudentsPlan.setYxdm(v);
+                        break;
+                    case 1:
+                        recruitStudentsPlan.setYxdh(v);
+                        break;
+                    case 2:
+                        recruitStudentsPlan.setYxdhmc(v);
+                        break;
+                    case 3:
+                        recruitStudentsPlan.setSzd(v);
+                        break;
+                    case 4:
+                        recruitStudentsPlan.setSf985(CommonUtils.convertStringToInteger(v));
+                        break;
+                    case 5:
+                        recruitStudentsPlan.setSf211(CommonUtils.convertStringToInteger(v));
+                        break;
+                    case 6:
+                        recruitStudentsPlan.setBxlxdm(CommonUtils.convertStringToInteger(v));
+                        break;
+                    case 7:
+                        recruitStudentsPlan.setBxlxmc(v);
+                        break;
+                    case 8:
+                        recruitStudentsPlan.setSsdm(CommonUtils.convertStringToInteger(v));
+                        break;
+                    case 9:
+                        recruitStudentsPlan.setSsmc(v);
+                        break;
+                    case 10:
+                        recruitStudentsPlan.setZgdm(CommonUtils.convertStringToInteger(v));
+                        break;
+                    case 11:
+                        recruitStudentsPlan.setZgmc(v);
+                        break;
+                    case 12:
+                        recruitStudentsPlan.setCcdm(CommonUtils.convertStringToInteger(v));
+                        break;
+                    case 13:
+                        recruitStudentsPlan.setCcmc(v);
+                        break;
+                    case 14:
+                        recruitStudentsPlan.setZydm(v);
+                        break;
+                    case 15:
+                        recruitStudentsPlan.setSbdm2(v);
+                        break;
+                    case 16:
+                        recruitStudentsPlan.setZymc(v);
+                        break;
+                    case 17:
+                        recruitStudentsPlan.setXzdmxg(v);
+                        break;
+                    case 18:
+                        recruitStudentsPlan.setXzdm(v);
+                        break;
+                    case 19:
+                        recruitStudentsPlan.setXzmc(v);
+                        break;
+                    case 20:
+                        recruitStudentsPlan.setSfbz(v);
+                        break;
+                    case 21:
+                        recruitStudentsPlan.setKldm(v);
+                        break;
+                    case 22:
+                        recruitStudentsPlan.setKsklmc(v);
+                        break;
+                    case 23:
+                        recruitStudentsPlan.setPcdm(v);
+                        break;
+                    case 24:
+                        recruitStudentsPlan.setPcmc(v);
+                        break;
+                    case 25:
+                        recruitStudentsPlan.setKslxdm(v);
+                        break;
+                    case 26:
+                        recruitStudentsPlan.setKslxmc(v);
+                        break;
+                    case 27:
+                        recruitStudentsPlan.setXkkmyq(v);
+                        break;
+                    case 28:
+                        recruitStudentsPlan.setXkkmyqzw(v);
+                        break;
+                    case 29:
+                        recruitStudentsPlan.setZsjhs(CommonUtils.convertStringToInteger(v));
+                        break;
+                    case 30:
+                        recruitStudentsPlan.setBz(v);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            recruitStudentsPlans.add(recruitStudentsPlan);
+        }
+        if (recruitStudentsPlans.size() > 0) {
+            int len = recruitStudentsPlans.size();
+            if (len <= Constants.EXCEL_BATCH_SIZE) {
+                recruitStudentsPlanMapper.insertRecruitStudentsPlanBatch(recruitStudentsPlans);
+            }
+            List<RecruitStudentsPlan> temp = new ArrayList<>(Constants.EXCEL_BATCH_SIZE);
+            for (int i = 0; i < len; i++) {
+                if (i > 0 && i % Constants.EXCEL_BATCH_SIZE == 0) {
+                    recruitStudentsPlanMapper.insertRecruitStudentsPlanBatch(temp);
+                    temp.clear();
+                }
+                temp.add(recruitStudentsPlans.get(i));
+            }
+            if (temp.size() > 0) {
+                recruitStudentsPlanMapper.insertRecruitStudentsPlanBatch(temp);
+            }
+        }
+        if (inp != null) {
+            inp.close();
+        }
+    }
+}
