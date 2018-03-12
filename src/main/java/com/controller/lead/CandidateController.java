8726449bdcf9e2e92786922844d1f07b183ee023
84548 <845486067@>
2018-03-12 22:17:54 +0800

    新增方法 1.查询考生列表（listCandidate）

---
+++ src/main/java/com/controller/lead/CandidateController.java
@@ -0,0 +1,57 @@
+package com.controller.lead;
+
+import com.common.result.Result;
+import com.entity.Candidate;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.service.CandidateService;
+import com.vo.LoginUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+/**
+ * 考生控制器
+ *
+ * @author 潘根山
+ * @create 2018-03-12 21:09
+ * @since 1.0.0
+ */
+@Controller
+@RequestMapping("/candidate")
+public class CandidateController {
+    @Autowired
+    private CandidateService candidateService;
+
+    @RequestMapping("/")
+    public String toCandidate() {
+        return "lead/candidate";
+    }
+
+    /**
+     * 查询考生列表
+     *
+     * @param loginUser   登录用户
+     * @param candidate   考生对象
+     * @param pageSize    每页显示的条数
+     * @param currentPage 当前页
+     * @return 考生列表
+     */
+    @RequestMapping("/list")
+    @ResponseBody
+    public Result listCandidate(
+            LoginUser loginUser
+            , Candidate candidate
+            , @RequestParam("pageSize") int pageSize
+            , @RequestParam("currentPage") int currentPage) {
+        candidate.setConsultantId(loginUser.getLoginId());
+        PageHelper.startPage(currentPage, pageSize);
+        List<Candidate> candidates = candidateService.listCandidateByCondition(candidate);
+        PageInfo<Candidate> pageInfo = new PageInfo<>(candidates);
+        return Result.page(pageInfo.getList(), pageInfo.getTotal());
+    }
+}
