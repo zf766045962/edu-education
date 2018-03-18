6ba72fa4c2edd00f12ca8232982fc9e56e2ceb74
84548 <845486067@>
2018-03-18 20:56:09 +0800

    往年投档情况表repository

---
+++ src/main/java/com/dao/WntdqkMapper.java
@@ -0,0 +1,23 @@
+package com.dao;
+
+import com.entity.Wntdqk;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * 往年投档情况 repository
+ *
+ * @author 潘根山
+ * @create 2018-02-12 09:58
+ * @since 1.0.0
+ */
+@Repository
+public interface WntdqkMapper {
+    /**
+     * 批量插入往年投档情况
+     *
+     * @param list
+     */
+    void insertWntdqkBatch(List<Wntdqk> list);
+}
