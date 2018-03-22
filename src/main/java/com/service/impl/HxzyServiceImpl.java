d2b06b66a754a0a1687a2365fe0aa11ae8f8ea17
84548 <845486067@>
2018-03-22 22:35:04 +0800

    新增候志愿serviceImpl

---
+++ src/main/java/com/service/impl/HxzyServiceImpl.java
@@ -0,0 +1,40 @@
+package com.service.impl;
+
+import com.dao.HxzyMapper;
+import com.entity.Hxzy;
+import com.service.HxzyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author 潘根山
+ * @create 2018-03-22 22:11
+ * @since 1.0.0
+ */
+@Service
+public class HxzyServiceImpl implements HxzyService {
+    @Autowired
+    private HxzyMapper hxzyMapper;
+
+    @Override
+    public int insertSelective(Hxzy record) {
+        return hxzyMapper.insertSelective(record);
+    }
+
+    @Override
+    public List<Hxzy> listHxzyByCandidateId(Long candidateId) {
+        return hxzyMapper.listHxzyByCandidateId(candidateId);
+    }
+
+    @Override
+    public void deleteAllByCandidateId(Long candidateId) {
+        hxzyMapper.deleteAllByCandidateId(candidateId);
+    }
+
+    @Override
+    public void deleteByIds(List<Long> ids) {
+        hxzyMapper.deleteByIds(ids);
+    }
+}
