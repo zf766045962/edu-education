256e4fad04f3db19a55f170b7c9338e9d3b95b61
84548 <845486067@>
2018-03-26 22:17:49 +0800

    新增方法 1.countSchool 根据条件计算 冲稳保学校数量 2.countMajor 根据条件计算 冲稳保专业数量 3.listRecruitStudentsPlan 根据条件查询 详细的专业情况

--- src/main/java/com/dao/RecruitStudentsPlanMapper.java
+++ src/main/java/com/dao/RecruitStudentsPlanMapper.java
@@ -1,9 +1,11 @@
 package com.dao;
 
 import com.entity.RecruitStudentsPlan;
+import com.vo.RecruitStudentsPlanVo;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * 计划表repository
@@ -14,14 +16,40 @@ import java.util.List;
  */
 @Repository
 public interface RecruitStudentsPlanMapper {
-    int insertSelective(RecruitStudentsPlan record);
-
-    int updateByPrimaryKeySelective(RecruitStudentsPlan record);
-
     /**
      * 批量插入计划数据
      *
      * @param list
      */
     void insertRecruitStudentsPlanBatch(List<RecruitStudentsPlan> list);
+
+    /**
+     * 根据条件计算 冲稳保学校数量
+     *
+     * @param map nf 年份
+     *            szds 所在地集合
+     *            kms 科目集合
+     * @return 学校数量
+     */
+    int countSchool(Map<String, Object> map);
+
+    /**
+     * 根据条件计算 冲稳保专业数量
+     *
+     * @param map nf 年份
+     *            szds 所在地集合
+     *            kms 科目集合
+     * @return 专业数量
+     */
+    int countMajor(Map<String, Object> map);
+
+    /**
+     * 根据条件查询 详细的专业情况
+     *
+     * @param map nf 年份
+     *            szds 所在地集合
+     *            kms 科目集合
+     * @return 专业详情
+     */
+    List<RecruitStudentsPlanVo> listRecruitStudentsPlan(Map<String, Object> map);
 }
