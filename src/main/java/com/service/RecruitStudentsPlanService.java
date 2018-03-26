3b5fdf32994bfabd5ff7dd0a1533d4107288933d
84548 <845486067@>
2018-03-26 22:19:35 +0800

    新增方法 1.countSchool 根据条件计算 冲稳保学校数量 2.countMajor 根据条件计算 冲稳保专业数量 3.listRecruitStudentsPlan 根据条件查询 详细的专业情况

--- src/main/java/com/service/RecruitStudentsPlanService.java
+++ src/main/java/com/service/RecruitStudentsPlanService.java
@@ -1,9 +1,12 @@
 package com.service;
 
 import com.util.exception.CustomException;
+import com.vo.RecruitStudentsPlanVo;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author 潘根山
@@ -19,4 +22,31 @@ public interface RecruitStudentsPlanService {
      * @throws CustomException CustomException
      */
     void uploadData(MultipartFile file) throws IOException, CustomException;
+    /**
+     * 根据条件计算 冲稳保学校数量
+     *
+     * @param map nf 年份
+     *            szds 所在地集合
+     *            kms 科目集合
+     * @return 学校数量
+     */
+    int countSchool(Map<String, Object> map);
+    /**
+     * 根据条件计算 冲稳保专业数量
+     *
+     * @param map nf 年份
+     *            szds 所在地集合
+     *            kms 科目集合
+     * @return 专业数量
+     */
+    int countMajor(Map<String, Object> map);
+    /**
+     * 根据条件查询 详细的专业情况
+     *
+     * @param map nf 年份
+     *            szds 所在地集合
+     *            kms 科目集合
+     * @return 专业详情
+     */
+    List<RecruitStudentsPlanVo> listRecruitStudentsPlan(Map<String, Object> map);
 }
