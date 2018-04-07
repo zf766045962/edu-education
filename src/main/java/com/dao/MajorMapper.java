5ee94e94eca8a5c2180b8418a4f0d0adedb1ee39
Sam <845486067@qq.com>
2018-04-08 06:18:40 +0800

    新增专业表Repository

---
+++ src/main/java/com/dao/MajorMapper.java
@@ -0,0 +1,26 @@
+package com.dao;
+
+import com.entity.Major;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * 专业表repository
+ *
+ * @author 潘根山
+ * @create 2018-03-22 22:01
+ * @since 1.0.0
+ */
+@Repository
+public interface MajorMapper {
+    /**
+     * 查询所有的专业信息
+     *
+     * @param zymc 专业名称
+     * @return 专业信息
+     */
+
+    List<Major> listMajor(@Param("zymc") String zymc);
+}
