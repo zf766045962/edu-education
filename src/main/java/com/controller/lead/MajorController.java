c1ff81ec5a5d33a38972136fd0fba0e09f08e584
Sam <845486067@qq.com>
2018-04-08 06:20:15 +0800

    新增专业表controller

---
+++ src/main/java/com/controller/lead/MajorController.java
@@ -0,0 +1,33 @@
+package com.controller.lead;
+
+import com.common.result.Result;
+import com.entity.Major;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.service.MajorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/major")
+public class MajorController {
+    @Autowired
+    private MajorService majorService;
+
+    @GetMapping("/")
+    @ResponseBody
+    public Result findMajor(
+             @RequestParam(value = "pageSize", defaultValue = "10") int pageSize
+            , @RequestParam(value = "currentPage", defaultValue = "1") int currentPage, String zymc) {
+        PageHelper.startPage(currentPage, pageSize);
+        List<Major> majorList = majorService.listMajor(zymc);
+        PageInfo<Major> pageInfo = new PageInfo<>(majorList);
+        return Result.page(pageInfo.getList(), pageInfo.getTotal());
+    }
+}
