38544a6c218ef0667f7219cd295bf1e857e6deef
pgs <845486067@qq.com>
2018-04-26 22:19:37 +0800

    新增往年投档汇总情况serviceImpl

---
+++ src/main/java/com/service/impl/WntdqkHzServiceImpl.java
@@ -0,0 +1,26 @@
+package com.service.impl;
+
+import com.dao.WntdqkHzMapper;
+import com.service.WntdqkHzService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author 潘根山
+ * @create 2018-04-26 21:18
+ * @since 1.0.0
+ */
+@Service
+public class WntdqkHzServiceImpl implements WntdqkHzService {
+    @Autowired
+    private WntdqkHzMapper wntdqkHzMapper;
+
+    @Override
+    public void createWntdqkHz(String year) {
+        // 清空往年投档数据临时表
+        //todo 向往年投档数据临时表中插入选择年的投档数据
+        //todo 将选择年的投档数据与前两年的数据进行比较，计算参考指数和名次
+        wntdqkHzMapper.deleteWntdqkHz();
+        wntdqkHzMapper.insertWntdqkHz(year);
+    }
+}
