bc57a47a03a5c24fcb7bda851b56d75862db0519
pgs <845486067@qq.com>
2018-05-16 23:03:55 +0800

    完善临时数据生成流程

--- src/main/java/com/service/impl/WntdqkHzServiceImpl.java
+++ src/main/java/com/service/impl/WntdqkHzServiceImpl.java
@@ -2,6 +2,9 @@ package com.service.impl;
 
 import com.dao.WntdqkHzMapper;
 import com.service.WntdqkHzService;
+import com.service.WntdqkService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,18 +15,42 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class WntdqkHzServiceImpl implements WntdqkHzService {
+    private static final Logger logger = LoggerFactory.getLogger(WntdqkHzService.class);
     @Autowired
     private WntdqkHzMapper wntdqkHzMapper;
+    @Autowired
+    private WntdqkService wntdqkService;
 
     @Override
     public void createWntdqkHz(String year) {
         // 清空往年投档数据临时表
         // 向往年投档数据临时表中插入选择年的投档数据
-        //todo 将选择年的投档数据与前两年的数据进行比较，计算参考指数和名次
+        long startTime = System.currentTimeMillis();
+        logger.info("开始生成投档临时数据");
+        logger.info("开始删除投档临时数据");
         wntdqkHzMapper.deleteWntdqkHz();
+        logger.info("投档临时数据删除完毕");
+        logger.info("开始同步{}年投档数据备注", year);
+        wntdqkService.updateBz(year);
+        logger.info("{}年投档数据备注同步完成", year);
+        // 生成段数描述数据
+        logger.info("开始生成段数描述数据");
+        wntdqkService.generateDsms(year);
+        logger.info("段数描述数据生成完毕");
+        // 更新段数描述数据
+        logger.info("开始更新段数描述数据");
+        wntdqkService.updateDsms(year);
+        logger.info("更新段数描述数据完毕");
+        logger.info("更新段数描述数据完毕");
+        // 清空临时表
+        logger.info("开始删除投档临时数据");
+        wntdqkHzMapper.deleteWntdqkHz();
+        logger.info("投档临时数据删除完毕");
+        // 将正式表的数据插入到临时表中
+        logger.info("开始更新投档数据临时表数据");
         wntdqkHzMapper.insertWntdqkHz(year);
-        int iYear = Integer.parseInt(year);
-        String lastYear = "" + (iYear - 1);
-        String beforeLastYear = "" + (iYear - 2);
+        logger.info("更新投档数据临时表数据完毕");
+        long endTime = System.currentTimeMillis();
+        logger.info("生成完毕，共计用时:{}s", (double) (endTime - startTime) / 1000);
     }
 }
