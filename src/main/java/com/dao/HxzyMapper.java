205249f55eb638e0b851aa57f54f8fac1a320d7a
pgs <845486067@qq.com>
2018-06-27 06:59:57 +0800

    1.deleteByCandidateIdAndSchoolCodeAndMajorCode 增加 referenceIndex

--- src/main/java/com/dao/HxzyMapper.java
+++ src/main/java/com/dao/HxzyMapper.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -50,13 +51,15 @@ public interface HxzyMapper {
     /**
      * 根据考生Id，学校代码，专业代码取消候选志愿
      *
-     * @param candidateId 考生ID
-     * @param schoolCode  学校代码
-     * @param majorCode   专业代码
+     * @param candidateId    考生ID
+     * @param schoolCode     学校代码
+     * @param majorCode      专业代码
+     * @param referenceIndex 参考指数
      */
-    @Delete("delete from hxzy where candidate_id=#{candidateId} and school_code=#{schoolCode} and major_code_new=#{majorCode}")
+    @Delete("delete from hxzy where candidate_id=#{candidateId} and school_code=#{schoolCode} and major_code_new=#{majorCode} and reference_index=#{referenceIndex}")
     void deleteByCandidateIdAndSchoolCodeAndMajorCode(
             @Param("candidateId") long candidateId
             , @Param("schoolCode") String schoolCode
-            , @Param("majorCode") String majorCode);
+            , @Param("majorCode") String majorCode
+            , @Param("referenceIndex") BigDecimal referenceIndex);
 }
