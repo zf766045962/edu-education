0b023479f3a4b0bd7fc83f3bed0cdb95e4931e30
pgs <845486067@qq.com>
2018-07-18 20:44:13 +0800

    新增 1.findZymc 通过备注查询专业信息 2.deleteWntdqkTemp 删除临时表数据 3.insertTempToReal 将临时数插入到原始表中 4.deleteWntdqkByBz 根据备注删除数据

--- src/main/java/com/service/WntdqkService.java
+++ src/main/java/com/service/WntdqkService.java
@@ -1,9 +1,12 @@
 package com.service;
 
+import com.entity.Wntdqk;
 import com.util.exception.CustomException;
+import org.apache.ibatis.annotations.Delete;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * @author 潘根山
@@ -20,6 +23,7 @@ public interface WntdqkService {
      * @throws CustomException CustomException
      */
     void uploadData(MultipartFile file, String year) throws IOException, CustomException;
+
     /**
      * 生成段数描述的临时数据
      *
@@ -33,10 +37,37 @@ public interface WntdqkService {
      * @param year 年份
      */
     void updateDsms(String year);
+
     /**
      * 通过高考计划库，更新往年投档数据中的备注
      *
      * @param year 年份
      */
-    void updateBz(String year);
+    void updateBz(String year, String planTime);
+
+    /**
+     * 通过备注查询专业信息
+     *
+     * @param year 年份
+     */
+    List<Wntdqk> findZymc(String year);
+
+    /**
+     * 删除临时表数据
+     */
+    void deleteWntdqkTemp();
+
+    /**
+     * 将临时数插入到原始表中
+     */
+    void insertTempToReal();
+
+    /**
+     * 根据备注删除数据
+     *
+     * @param year 年份
+     */
+    void deleteWntdqkByBz(String year);
+
+    void insertWntdqkTempBatch(List<Wntdqk> tempList);
 }
