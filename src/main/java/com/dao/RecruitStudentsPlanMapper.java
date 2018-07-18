b54a86ea54718f2cf8b6dacff5e3633c5087a4da
pgs <845486067@qq.com>
2018-07-18 20:35:06 +0800

    新增 1.deleteRecruitStudentsPlan 根据年份删除招生计划库数据\ 2.findZymc 根据备注查询专业信息 3.insertRecruitStudentsPlanTempBatch 批量插入数据 4.deleteRecruitStudentsPlanTemp 删除临时表数据 5.updateSyzsjhsByTemp 将符合条件的正式数据剩余招生计划数更新 6.updateSyzsjhsToZero 将不符合条件的剩余招生计划数更新为0

--- src/main/java/com/dao/RecruitStudentsPlanMapper.java
+++ src/main/java/com/dao/RecruitStudentsPlanMapper.java
@@ -3,6 +3,7 @@ package com.dao;
 import com.entity.RecruitStudentsPlan;
 import com.vo.RecruitStudentsPlanVo;
 import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -55,8 +56,53 @@ public interface RecruitStudentsPlanMapper {
     List<RecruitStudentsPlanVo> listRecruitStudentsPlan(Map<String, Object> map);
 
     /**
-     * 清空招生计划库数据
+     * 根据年份删除招生计划库数据
+     *
+     * @param nf 年份
      */
-    @Delete("delete from recruit_students_plan")
-    void deleteRecruitStudentsPlan();
+    @Delete("delete from recruit_students_plan where nf=#{nf}")
+    void deleteRecruitStudentsPlan(String nf);
+
+    /**
+     * 根据备注查询专业信息
+     *
+     * @param year 年份
+     * @return 专业信息
+     */
+    List<RecruitStudentsPlan> findZymc(@Param("year") String year);
+
+    /**
+     * 批量插入数据
+     *
+     * @param list
+     */
+    void insertRecruitStudentsPlanTempBatch(List<RecruitStudentsPlan> list);
+
+    /**
+     * 根据备注删除专业情况
+     *
+     * @param year 年份
+     */
+    void deleteRecruitStudentsPlanByBz(@Param("year") String year);
+
+    /**
+     * 将临时数据插入到原始表中
+     */
+    void insertTempToReal();
+
+    /**
+     * 删除临时表数据
+     */
+    @Delete("delete from recruit_students_plan_temp")
+    void deleteRecruitStudentsPlanTemp();
+
+    /**
+     * 将符合条件的正式数据剩余招生计划数更新
+     */
+    void updateSyzsjhsByTemp(@Param("year") String year);
+
+    /**
+     * 将不符合条件的剩余招生计划数更新为0
+     */
+    void updateSyzsjhsToZero(@Param("year") String year);
 }
